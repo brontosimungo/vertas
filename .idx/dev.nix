7d579550cb238e93932b1fa24a62943736b2f393
brontosimungo <brontosimungo@outlook.co.id>
2026-05-10 17:01:20 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,6 +8,8 @@
   packages = [
     pkgs.python312
     pkgs.python312Packages.pip
+    pkgs.python312Packages.urllib3 # Library langsung diurus oleh Nix
+    pkgs.python312Packages.requests
   ];
 
   # Sets environment variables in the workspace
